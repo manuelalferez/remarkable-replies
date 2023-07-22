b7ecaf7f03feb4db4c659adb6379ef263d713ae2
Manuel Alférez <manuelalferezruiz@gmail.com>
2023-07-22 18:46:08 +0300

    refactor: Renaming kv functions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,12 +3,17 @@ import "server-only";
 import { kv } from "@vercel/kv";
 import { getTimeStamp } from "./utils";
 
-export async function pushToList(key: string, comment: string) {
+export async function setComment(key: string, comment: string) {
   const timeStamp = getTimeStamp();
-  await kv.lpush(key, { comment, timeStamp });
+  const id = new Date().valueOf().toString();
+  await kv.lpush(key, { id, comment, timeStamp });
 }
 
-export async function getList(key: string) {
-  const list = await kv.lrange(key, 0, -1);
-  return list;
+export async function getComments(key: string) {
+  const comments = await kv.lrange(key, 0, -1);
+  return comments;
+}
+
+export async function delLastComment(key: string) {
+  await kv.lpop(key);
 }
